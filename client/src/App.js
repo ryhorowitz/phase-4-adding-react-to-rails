0821ce7acf9c64f36957bcdc9c225894d0ec40c4
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-31 13:29:46 -0400

    complete

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
